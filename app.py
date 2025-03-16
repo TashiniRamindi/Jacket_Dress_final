286fe43afdc9e36a64f96e415d51783df2775f82
TashiniRamindi <tashiniramindi@gmail.com>
2025-03-16 14:14:20 +0530

    app.py

--- app.py
+++ app.py
@@ -154,6 +154,10 @@ elif cloth_type == 'Jacket':
         'Lightweight': st.radio('Is the jacket lightweight?', ('Yes', 'No'), index=None),
         'Water_Repellent': st.radio('Is the jacket water repellent?', ('Yes', 'No'), index=None)
     }
+
+# Mapping for seasons
+season_mapping = {0: 'spring', 1: 'summer', 2: 'winter', 3: 'autumn'}
+
   
 
 if st.button("Predict"):
